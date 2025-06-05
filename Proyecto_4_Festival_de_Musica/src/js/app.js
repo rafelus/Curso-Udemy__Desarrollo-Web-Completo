fa28be4a9fa44bc3208d84001085347a637c5fb4
rafelus <ravazsan1@outlook.es>
2025-06-05 17:55:48 +0200

    Proyecto4_dia_5

--- Proyecto_4_Festival_de_Musica/src/js/app.js
+++ Proyecto_4_Festival_de_Musica/src/js/app.js
@@ -22,9 +22,11 @@ function crearGaleria(){
     const cantidaImagenes = 16
     const galeria = document.querySelector('.galeria-imagenes')
     for(let i = 1; i<=cantidaImagenes; i++){
-        const imagen = document.createElement('IMG')
-        imagen.src = `src/img/gallery/full/${i}.jpg`
-        imagen.alt = 'imagen galeria'
+        const imagen = document.createElement('PICTURE')
+        imagen.innerHTML = `
+            <source srcset="build/img/gallery/thumb/${i}.avif" type="image/avif">
+            <source srcset="build/img/gallery/thumb/${i}.webp" type="image/webp">
+            <img loading="lazy" width="200" height="300" src="build/img/gallery/thumb/${i}.jpg" alt="imagen galeria">`;
         // Event handler (mostrar imgen modal)
         imagen.onclick = function() {
             mostrarImagen(i)
@@ -34,9 +36,11 @@ function crearGaleria(){
 }
 
 function mostrarImagen(i){
-    const imagen = document.createElement('IMG')
-    imagen.src = `src/img/gallery/full/${i}.jpg`
-    imagen.alt = 'imagen galeria'
+    const imagen = document.createElement('PICTURE')
+    imagen.innerHTML = `
+        <source srcset="build/img/gallery/full/${i}.avif" type="image/avif">
+        <source srcset="build/img/gallery/full/${i}.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/img/gallery/full/${i}.jpg" alt="imagen galeria">`;
 
     // Genera modal
     const modal = document.createElement('DIV')
